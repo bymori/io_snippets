e8bca95e48493c48e56c0475ecdd00e2b9c58955
by_mori <by_mori@163.com>
2022-04-03 10:45:54 +0800

    var和function提升

---
+++ src/JavaScript/setTimeoutArgN/var和function提升.js
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-04-03 10:36:14
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-04-03 10:45:48
+ */
+var a = 1;
+
+var a = function () {
+  return console.log(2);
+};
+
+function a() {
+  return console.log(3);
+}
+
+console.log(a); // 2
